0053df411c1706d198aef45bb29bfd70931072ac
Prannoy Kumar <prannoykumar51@gmail.com>
2015-11-04 07:40:06 +0530

    First commit

---
+++ WebContent/file2.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+
+$(document).ready(function()
+{
+    $('.start').click(function(){
+        var o=new MyNewClass();
+        function a()
+        {
+            $.ajax(
+            {
+                url: '/echo/html/',
+                success: function(data) 
+                {
+                    alert('new complete');
+                    o.markDone();
+                }
+            })
+
+        };
+        o.register(a);
+        o.start();
+    });
+});
